14e5a04216912101fdc8905026defd621963627e
MahanGholami <Mahan.roy79@gmail.com>
2024-05-14 02:28:58 +0330

    Qgis

---
+++ data/RoadLayer_1.js
@@ -0,0 +1,11 @@
+var json_RoadLayer_1 = {
+"type": "FeatureCollection",
+"name": "RoadLayer_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Road": "M1", "Length": 544.537, "Width": 8.238, "Name": "ShahidMadani" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 50.014899200989646, 36.292192363122894 ], [ 50.014085455969592, 36.291330337066569 ], [ 50.011911594273187, 36.287938359467454 ] ] ] } },
+{ "type": "Feature", "properties": { "Road": "M2", "Length": 580.755, "Width": 17.767, "Name": "Sortak" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 50.017369498371977, 36.290983649988966 ], [ 50.01566063382986, 36.289971689708658 ], [ 50.014242392509196, 36.289156489938158 ], [ 50.013265898485137, 36.288388132932567 ], [ 50.012510278109367, 36.287582286968991 ] ] ] } },
+{ "type": "Feature", "properties": { "Road": "F1", "Length": 141.9, "Width": 9.37, "Name": "TajAhmadi" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 50.01335163233545, 36.290182515849366 ], [ 50.014916638382935, 36.289545350661143 ] ] ] } },
+{ "type": "Feature", "properties": { "Road": "F2", "Length": 121.525, "Width": 9.52, "Name": "Toos" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 50.01315691477707, 36.289881502796696 ], [ 50.01451703145343, 36.289314611249047 ] ] ] } }
+]
+}
